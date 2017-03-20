8419cfef66f193836f4d9de63efbad88b17968d6
Luke Hammond <luke.john.hammond@gmail.com>
2017-03-20 19:00:44 +0000

    dryed pangram code slightly

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -3,18 +3,16 @@ const Pangram = function (sentence) {
 };
 
 Pangram.prototype.isPangram = function() {
+  if (!this.sentence) return false;
   const sentence = this.sentence
     .toLowerCase()
     .replace(/[^a-z]/g, '')
     .split('')
     .sort();
 
-  const uniqueCharacters = Array.from(new Set(sentence));
-  if (!sentence) return false;
-  return (uniqueCharacters.length < 26) ? false : true;
+  const uniqueCharacters = new Set(sentence);
+
+  return (uniqueCharacters.size < 26) ? false : true;
 };
 
 module.exports = Pangram;
-
-var pangram = new Pangram('a quick movement of the enemy will jeopardize five gunboats!_£$£$%£$%£^');
-pangram.isPangram();
